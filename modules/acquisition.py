5d967a7cae9b09a9043f152c95f6ef40a1d687a0
AlanaCastillo <141866356+AlanaCastillo@users.noreply.github.com>
2023-11-24 18:55:51 +0100

    Add files via upload

---
+++ modules/acquisition.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def bicimad_dataframe():
+    #print("Esta es la estación de BICIMAD")
+    df_mad_final = pd.read_csv('./modules/bicimad_project.csv')
+    #print(df_mad_final)
+    return df_mad_final
+
+bicimad_dataframe()
+
+
+def bicipark_dataframe():
+    #print("Esta es la estación de BICIPARK")
+    df_park_final = pd.read_csv('./modules/bicipark_project.csv')
+    #print(df_park_final)
+    return df_park_final
+
+bicipark_dataframe()
+    
+
